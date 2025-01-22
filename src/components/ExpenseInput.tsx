b9ac78155c195aee69815627e1a90254a601cff9
Jisan Jung <jjjung13@gmail.com>
2025-01-22 00:19:34 -0600

    add input for expense

--- src/components/ExpenseInput.tsx
+++ src/components/ExpenseInput.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 
 const ExpenseInput = () => {
 
+    const [expense, setExpense] = useState('0');
+
   return (
-    <div>
-        <h1>
-            <span>$</span>
-        </h1>
+    <div className="flex justify-center">
+        <input 
+            type="text" 
+            inputMode="numeric" 
+            value={expense} 
+            onChange={(e) => setExpense(e.target.value)}
+            className="text-center text-7xl w-full py-2 my-12"
+            autoFocus={true}
+        />
     </div>
   )
 }
