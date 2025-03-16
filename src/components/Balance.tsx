41324fb6037698ca643c53e46f1888ffbcff4955
Jisan Jung <jjjung13@gmail.com>
2025-03-16 13:47:26 -0500

    shimmer loading

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+import ShimmerLoading from "./ShimmerLoading"
 
 const Balance = () => {
+  
+  const [loading, setLoading] = useState(false);
+
   return (
     <>
-      <p className="text-2xl font-bold text-emerald-600">
-            <span>$</span>
-            <span>1000</span>
+      {
+        loading ? 
+        <ShimmerLoading/> :
+        <p className="text-2xl font-bold text-emerald-600">
+          <span>$</span>
+          <span>1000</span>
         </p>
+      }
     </>
   )
 }
---
+++ src/components/ShimmerLoading.tsx
@@ -0,0 +1,7 @@
+const ShimmerLoading = () => {
+  return (
+    <div className='shimmer-effect w-24 h-6 rounded-full'></div>
+  )
+}
+
+export default ShimmerLoading
