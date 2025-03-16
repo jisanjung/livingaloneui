21dccf4c2d98e933b6f500d0a68090764b291249
Jisan Jung <jjjung13@gmail.com>
2025-03-16 18:51:56 -0500

    revert passcode page

--- src/components/ExpenseAppWrapper.tsx
+++ src/components/ExpenseAppWrapper.tsx
@@ -1,21 +1,19 @@
-// import { useStoreState } from "../model"
-// import ConfirmDrawer from "./ConfirmDrawer"
-// import ExpenseInput from "./ExpenseInput"
-// import Header from "./Header"
-// import Keypad from "./Keypad"
-import PasscodePage from "./PasscodePage"
+import { useStoreState } from "../model"
+import ConfirmDrawer from "./ConfirmDrawer"
+import ExpenseInput from "./ExpenseInput"
+import Header from "./Header"
+import Keypad from "./Keypad"
 
 const ExpenseAppWrapper = () => {
 
-    // const preConfirmTriggered = useStoreState(state => state.preConfirm.triggered);
+    const preConfirmTriggered = useStoreState(state => state.preConfirm.triggered);
 
   return (
     <main className="p-3">
-        <PasscodePage/>
-        {/* {preConfirmTriggered && <ConfirmDrawer/>}
+        {preConfirmTriggered && <ConfirmDrawer/>}
         <Header/>
         <ExpenseInput/>
-        <Keypad/> */}
+        <Keypad/>
     </main>
   )
 }
