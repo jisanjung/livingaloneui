6e0b9962bfd9c9f0823977bdc049b658dcf1e287
Jisan Jung <jjjung13@gmail.com>
2025-03-16 13:31:56 -0500

    open drawer animation

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import { useStoreState } from "../model";
 import { EXPENSE_NAMES } from "../constants";
@@ -9,14 +9,24 @@ const ConfirmModal = () => {
 
     const [loading, setLoading] = useState(false);
     const [confirmed, setConfirmed] = useState(false);
+    const [openAnimation, setOpenAnimation] = useState(false);
     const currentExpenseInput = useStoreState(state => state.expense.current);
     const currentExpenseName = useStoreState(state => state.expenseName.current);
 
     const parsedExpenseInput = parseInt(currentExpenseInput, 10); // remove leading 0's
     const currencyConverted = convertToCurrency(parsedExpenseInput);
 
+    useEffect(() => {
+        setOpenAnimation(true);
+    }, []);
+
   return (
-    <div className="fixed bottom-0 right-0 left-0 bg-white py-6 px-3 rounded-t-3xl">
+    <div className="fixed right-0 left-0 bg-white py-6 px-3 rounded-t-3xl"
+        style={{
+            bottom: openAnimation ? '0' : '-185px',
+            transition: 'bottom 0.25s',
+        }}
+    >
         <div className="text-center pb-6">
             <h1 className="font-bold text-4xl">
                 $<span>{currencyConverted}</span>
