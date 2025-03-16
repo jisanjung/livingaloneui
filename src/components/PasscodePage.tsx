eb96558aceed35d1d919d5611a9a52e510686e66
Jisan Jung <jjjung13@gmail.com>
2025-03-16 18:48:51 -0500

    try adding click to input

--- src/components/PasscodePage.tsx
+++ src/components/PasscodePage.tsx
@@ -3,10 +3,11 @@ import { useEffect, useRef } from "react"
 const PasscodePage = () => {
 
     const inputRef = useRef<HTMLInputElement>(null);
-    
+
     useEffect(() => {
         if (inputRef.current) {
             inputRef.current.focus();
+            inputRef.current.click();
         }
     }, []);
 
