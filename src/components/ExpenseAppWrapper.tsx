1180f756c62a4a3b195619aeaec134b61117393f
Jisan Jung <jjjung13@gmail.com>
2025-03-16 18:40:22 -0500

    allow build to complete

--- src/components/ExpenseAppWrapper.tsx
+++ src/components/ExpenseAppWrapper.tsx
@@ -1,13 +1,13 @@
-import { useStoreState } from "../model"
-import ConfirmDrawer from "./ConfirmDrawer"
-import ExpenseInput from "./ExpenseInput"
-import Header from "./Header"
-import Keypad from "./Keypad"
+// import { useStoreState } from "../model"
+// import ConfirmDrawer from "./ConfirmDrawer"
+// import ExpenseInput from "./ExpenseInput"
+// import Header from "./Header"
+// import Keypad from "./Keypad"
 import PasscodePage from "./PasscodePage"
 
 const ExpenseAppWrapper = () => {
 
-    const preConfirmTriggered = useStoreState(state => state.preConfirm.triggered);
+    // const preConfirmTriggered = useStoreState(state => state.preConfirm.triggered);
 
   return (
     <main className="p-3">
